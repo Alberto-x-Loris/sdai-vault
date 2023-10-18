8d78667b57f9844eee6cfd281e2663561fd551d0
Alberto <albicento.ac@gmail.com>
2023-10-18 20:22:16 +0200

    fix: corrected version causing compilation to fail

--- src/periphery/uniswap/ISwapRouter.sol
+++ src/periphery/uniswap/ISwapRouter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity 0.8.20;
+pragma solidity 0.8.18;
 
 /// @title Router token swapping functionality
 /// @notice Functions for swapping tokens via Uniswap V3
