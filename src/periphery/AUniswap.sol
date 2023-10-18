84693b5096ae697fd69ce1efa27202cc64044ae2
Alberto <albicento.ac@gmail.com>
2023-10-18 20:22:16 +0200

    fix: corrected version causing compilation to fail

--- src/periphery/AUniswap.sol
+++ src/periphery/AUniswap.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.20;
+pragma solidity 0.8.18;
 
 import {Errors} from "./Errors.sol";
 import {ISwapRouter} from "./uniswap/ISwapRouter.sol";
